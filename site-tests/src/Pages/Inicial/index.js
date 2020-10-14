897cfe688bb4d61470d255409bb6c1bfff1d1a0c
uArthurSPereira <arthursp7007@gmail.com>
2020-10-14 18:51:41 -0300

    Criando Telas

--- site-tests/src/Pages/Inicial/index.js
+++ site-tests/src/Pages/Inicial/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       Hello World!
+
+      <Link to="/pop-up" > Test Pop-Up </Link>
     </div>
   );
 }
