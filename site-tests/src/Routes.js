4c95a2176e3bba8462111334e9bdd83d73241d26
uArthurSPereira <arthursp7007@gmail.com>
2020-10-14 18:50:00 -0300

    Routes Done

---
+++ site-tests/src/Routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
+
+import Inicial from './Pages/Inicial'
+import PopUp from './Pages/Pop-Up'
+
+export default function Routes() {
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route path="*" exact={true} component={Inicial} />
+                <Route path="/pop-up" component={PopUp} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
