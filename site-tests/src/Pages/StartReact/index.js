8de5061ad9f357701579b18e7b52caa338beb74c
uArthurSPereira <arthursp7007@gmail.com>
2020-10-15 12:31:21 -0300

    Test Color Label

---
+++ site-tests/src/Pages/StartReact/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import "./StartReact.css";
+
+export default function StartReact() {
+
+    return (
+        <div className="StartReact">
+            <div className="Color"></div>
+
+            <div className="Select-Color">
+                <button className="Vermelho"></button>
+                <button className="Laranja"></button>
+                <button className="Amarelo"></button>
+                <button className="Verde"></button>
+                <button className="Azul"></button>
+                <button className="Roxo"></button>
+            </div>
+        </div>
+    )
+}
--- site-tests/src/Routes.js
+++ site-tests/src/Routes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Inicial from './Pages/Inicial'
+import StartRecat from './Pages/StartReact'
+import DragNDrop from './Pages/DragNDrop'
 import PopUp from './Pages/Pop-Up/'
 
 export default function Routes() {
@@ -9,7 +11,9 @@ export default function Routes() {
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact={true} component={Inicial} />
-                <Route path="/pop-up" component={PopUp} />
+                <Route path="/primeiro-contato-react" component={StartRecat} />
+                <Route path="/drag-n-drop" component={DragNDrop} />
+                <Route path="/pop-up" component={PopUp} />    
             </Switch>
         </BrowserRouter>
     )
